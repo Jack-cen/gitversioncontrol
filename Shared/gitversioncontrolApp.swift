289597d5080cb289c9d6aaaf076f6022498dde16
cendaolang <cendaolang@163.com>
2022-03-25 12:00:52 +0800

    Initial Commit

---
+++ Shared/gitversioncontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitversioncontrolApp.swift
+//  Shared
+//
+//  Created by ZllLori on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct gitversioncontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
